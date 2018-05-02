4d19b205f4228309f42b433f1fb2ed29e02a8d52
imowbray97 <isaiah.mowbray97@gmail.com>
2018-05-01 21:03:12 -0400

    first commit to Project by Isaiah Mowbray

---
+++ src/main/java/com/javachort3/project_weather_/ProjectWeatherApplication.java
@@ -0,0 +1,12 @@
+package com.javachort3.project_weather_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWeatherApplication.class, args);
+	}
+}
---
+++ src/test/java/com/javachort3/project_weather_/ProjectWeatherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javachort3.project_weather_;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectWeatherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
